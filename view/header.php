93cdbcef4d02ea51bf4cbe0b999a38f809d9272d
meryembel127 <mememeryam7092@gmail.com>
2025-06-13 16:27:15 +0100

    changes

---
+++ view/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   <link rel="stylesheet" href="/src/style.css">
+    
+</head>
+<body>
+    <>
+        <nav class="navbar">
+          <a href="#" class="logo"><img src="logo pfe.jpg" alt="">BEAUTY</a>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+        </nav>
+            
+        <nav class="navbar">
+          <a href="#" class="logo"><img src="Logo R Cosmetics (1).png" alt="">BEAUTY</a>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+        </nav>
+
+      </header>
+</body>
+</html>
---
+++ view/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    </head>
+<body>
+  
+        <nav class="navbar">
+          <a href="#" class="logo"><img src="logo pfe.jpg" alt="">BEAUTY</a>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+        </nav>
+      </header>
+       <div class="hero">
+      <div class="content">
+        <h1>Welcome to MR BEAUTY Your Destination for Premium Global Cosmetic Brands</h1>
+        <p>Discover a world of beauty with our exclusive selection of high-end cosmetic products from the most renowned international <br> At MR BEAUTY, we are dedicated to bringing you the latest in skincare,  <br>– all in one place. Whether you're looking for timeless classics or trending beauty essentials, <br>our curated collection guarantees authenticity, quality, and elegance with every purchase.</p>
+        <a href="produit.html" class="btn">Shop Now</a>
+      </div>
+
+     <header>
+        <nav class="navbar">
+          <a href="#" class="logo"><img src="Logo R Cosmetics (1).png" alt="">BEAUTY</a>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About Us</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div class="titre">
+        <h1>Home of Global Cosmetic Brands</h1>
+      </div>
+      <div id="boxs">
+        <div class="box">
+          <div class="imbox">
+          <a href="">  <img src="rpfe.jpg" alt=""></a>
+          </div>
+          <div class="text">
+           <a href=""> <h3 id="RP">La roche posay</h3></a>
+          </div>
+        </div>
+
+         <div class="box">
+          <div class="imbox">
+           <a href=""> <img src="vpfe.jpg" alt=""></a>
+          </div>
+          <div class="text">
+           <a href=""><h3>Vichy</h3></a> 
+          </div>
+        </div>
+
+         <div class="box">
+          <div class="imbox">
+           <a href=""><img src="cpfe.jpg" alt=""></a> 
+          </div>
+          <div class="text">
+           <a href=""> <h3>CeraVe</h3></a> 
+          </div>
+        </div>
+      </div>
+</body>
+</html>
+      
+</body>
+</html>
